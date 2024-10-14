5590170c9fded831bd2af1713f37b8de21ca49d5
AlikRamdoni <alik34201@gmail.com>
2024-10-14 17:12:23 +0700

    deleted folder

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/firman', function () {
-    return view('firman');
-});
-
-Route::get('/g', function () {
-    return view('gilar');
-});
