1a7f68cfd5cb2945fb691dd5cdc4b0d88fff6a9a
gilar <gilarpengayom002@gmail.com>
2024-10-21 20:33:09 +0700

    feat(ui): nambah card

---
+++ resources/views/ujicoba.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mile Cafe 32</title>
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400&display=swap" rel="stylesheet"> <!-- Mengimpor font Poppins -->
+</head>
+<body>
+    <div class="menu-box">
+        <div class="menu-image">
+            <img src="miegoreng.png" alt="Mie Goreng Jawa">
+        </div>
+        <div class="menu-description">
+            <p class="title">Mie Goreng Jawa</p>
+            <p class="description">Mie Goreng khas Jawa dengan kecap manis, sayuran, dan ayam, memberikan rasa gurih manis.</p>
+            <p class="price">Rp 18.000</p>
+            <button>tambah</button>
+        </div>
+    </div>
+
+    <div class="menu-box">
+        <div class="menu-image">
+            <img src="miegoreng.png" alt="Mie Goreng Jawa">
+        </div>
+        <div class="menu-description">
+            <p class="title">Mie Goreng Jawa</p>
+            <p class="description">Mie Goreng khas Jawa dengan kecap manis, sayuran, dan ayam, memberikan rasa gurih manis.</p>
+            <p class="price">Rp 18.000</p>
+        </div>
+    </div>
+
+    <div class="menu-box">
+        <div class="menu-image">
+            <img src="miegoreng.png" alt="Mie Goreng Jawa">
+        </div>
+        <div class="menu-description">
+            <p class="title">Mie Goreng Jawa</p>
+            <p class="description">Mie Goreng khas Jawa dengan kecap manis, sayuran, dan ayam, memberikan rasa gurih manis.</p>
+            <p class="price">Rp 18.000</p>
+        </div>
+    </div>
+
+</body>
+</html>
