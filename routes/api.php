670ce51c597b2d42675b1c3e117966a38ee6e217
Wirdii <mhafizhwirdiyan@gmail.com>
2024-10-29 15:27:51 +0700

    feat (backend): membuat method untuk login dan set middleware protection sanctum

--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,11 @@ use App\Http\Controllers\AuthController;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user-profile', function (Request $request) {
+        return auth()->user();
+    });
+
+    // Tambahkan route yang ingin dilindungi di sini
+});
