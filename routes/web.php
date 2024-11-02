ea09a573701fdce6aab379d92bebbe0a3bd84efb
gilar <gilarpengayom002@gmail.com>
2024-11-02 20:38:41 +0700

    feat(frontend):menambah halaman sukses dan gagal

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,15 @@ Route::get('/logout', [AuthController::class, 'logout']);
 Route::get('/dashboard-kasir', [PesananController::class, 'index']);
 Route::get('/dashboard-staff', [PesananController::class, 'index']);
 Route::get('/laporan', [LaporanController::class, 'index']);
+
+// sukses Route
+Route::get('/sukses', function () {
+    return view('sukses');
+});
+Route::post('/sukses', [AuthController::class, 'sukses'])->name('sukses'); // Post route for sukses
+
+// gagal Route
+Route::get('/gagal', function () {
+    return view('gagal');
+});
+Route::post('/gagal', [AuthController::class, 'gagal'])->name('gagal'); // Post route for gagal
